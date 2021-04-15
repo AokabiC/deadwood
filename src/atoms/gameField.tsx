795744a3c36251f435d41b38788bfc480855e9b6
AokabiC <syht0312@gmail.com>
2021-04-16 01:45:15 +0900

    feat: add field and ball

--- src/atoms/gameField.tsx
+++ src/atoms/gameField.tsx
@@ -1,23 +1,57 @@
-import Matter from "matter-js";
+import React from "react";
+import Matter, { Bodies, Composite, Engine, Render, Runner } from "matter-js";
 import { useEffect, useRef } from "react";
+import { Button } from "./Button";
 
 const GameField: React.FC = () => {
+  const engine = useRef(Matter.Engine.create());
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const engine = Matter.Engine.create();
-  const runner = Matter.Runner.create();
-  const boxA = Matter.Bodies.rectangle(400, 200, 80, 80);
 
   useEffect(() => {
-    const render = Matter.Render.create({
-      canvas: canvasRef.current ?? undefined,
-      engine,
+    const cw = 800;
+    const ch = 600;
+    if (canvasRef.current === null) return;
+    const render = Render.create({
+      canvas: canvasRef.current,
+      engine: engine.current,
+      options: {
+        width: cw,
+        height: ch,
+        wireframes: false,
+        background: "transparent",
+      },
     });
-    Matter.Composite.add(engine.world, boxA);
-    Matter.Render.run(render);
-    Matter.Runner.run(runner, engine);
-  });
 
-  return <canvas ref={canvasRef}></canvas>;
+    Composite.add(engine.current.world, [
+      Bodies.rectangle(cw / 2, -10, cw, 20, { isStatic: true }),
+      Bodies.rectangle(-10, ch / 2, 20, ch, { isStatic: true }),
+      Bodies.rectangle(cw / 2, ch + 10, cw, 20, { isStatic: true }),
+      Bodies.rectangle(cw + 10, ch / 2, 20, ch, { isStatic: true }),
+    ]);
+
+    Runner.run(engine.current);
+    Render.run(render);
+
+    return () => {
+      Render.stop(render);
+      Engine.clear(engine.current);
+      render.canvas.remove();
+    };
+  }, []);
+
+  const handleAddCircle = () => {
+    const ball = Bodies.circle(400, 200, 30, {
+      mass: 10,
+    });
+    Composite.add(engine.current.world, ball);
+  };
+
+  return (
+    <>
+      <canvas width={800} height={600} ref={canvasRef}></canvas>
+      <Button onClick={handleAddCircle}>Add</Button>
+    </>
+  );
 };
 
 export { GameField };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
-import { Button } from "atoms/button";
 import { GameField } from "atoms/gameField";
 
 const IndexPage = () => (
   <div>
     <GameField />
-    <Button fontSize={16}>hey</Button>
   </div>
 );
 export default IndexPage;
